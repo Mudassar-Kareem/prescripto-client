fb3b4e70be4ff6ae4e1c0e972ef5d849a0e9c028
Mudassar <mudassarkareem@gmail.com>
2024-11-15 17:11:49 +0500

    .

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import Banner from "../components/layout/Banner"
 import Footer from "../components/layout/Footer"
 import Header from "../components/layout/Header"
 import Navbar from "../components/layout/Navbar"
-import Speciality from "../components/layout/speciality"
+import Speciality from "../components/layout/Speciality"
 import TopDoctor from "../components/layout/TopDoctor"
 
 
